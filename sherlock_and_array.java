a432720d130cd690b62def10f0191937187f7ad4
Rajan Sainher <32580910+RajanSainher@users.noreply.github.com>
2020-06-02 17:39:47 +0530

    create sherlock_and_array.java

---
+++ sherlock_and_array.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the balancedSums function below.
+    static String balancedSums(List<Integer> arr) {
+        int sum = 0;
+        // get sum of all array index.
+        for (int i = 0; i < arr.size(); i++){
+            sum += arr.get(i);
+        }
+
+        // initially left sum is zero.
+        int leftSum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            //if left sum equals right sum print yes.
+            if (leftSum == (sum - leftSum - arr.get(i))) {
+                return "YES";
+            }
+
+            leftSum += arr.get(i);
+        }
+
+        return "NO";
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                String result = balancedSums(arr);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
